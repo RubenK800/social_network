e480af5b4ccd7b5718d51a8787d9a2a5a2dc0844
Ruben Kubalyan <kubalyan-ruben@mail.ru>
2021-11-02 12:08:59 +0400

    change getting inserted post's id functional with New inserting

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -21,16 +21,23 @@ class PostsController extends Controller
         $files = $request->file('img');
         $postBody = $request->get('post-body');
         if (!is_null($files)) {
-            Post::insert(['user_id' => Auth::id(), 'body' => $postBody, 'likes_count' => 0, 'comments_count' => 0, 'reposts_count' => 0]);
+            $post = new Post();
+            $post->user_id = Auth::id();
+            $post->body = $postBody;
+            $post->likes_count = 0;
+            $post->comments_count = 0;
+            $post->reposts_count = 0;
+            $post->save();
             foreach($files as $file) {
                 $file->storeAs('post_pics', $file->getClientOriginalName());
                 $directory = asset('storage/' . 'post_pics/' . $file->getClientOriginalName());
-                    //get the last inserted post_id
-                    $data = Post::latest('id')->first();
-                    $id = $data->id;
-                    PostImage::insert(['post_id' => $id, 'image_name' => $file->getClientOriginalName(), 'image_directory' => $directory,
-                        'image_size' => $file->getSize()]);
-                }
+                $postImage = new PostImage();
+                $postImage->post_id = $post->id;
+                $postImage->image_name = $file->getClientOriginalName();
+                $postImage->image_directory = $directory;
+                $postImage->image_size = $file->getSize();
+                $postImage->save();
+            }
         } else {
             if (!is_null($postBody)) {
                 Post::insert(['user_id' => Auth::id(), 'body' => $postBody, 'likes_count' => 0, 'comments_count' => 0, 'reposts_count' => 0]);
